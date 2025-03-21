b113a6ee229a947623be95192925ef55c5a5488f
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-21 16:18:48 +0100

    [UPD] Added 2nd function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 function reverseString (string){ // Creo una funzione per invertire la stringa
     return string.reverse(); // Ritorno il valore della stringa invertito
 } // Fine funzione
+
+function palindrome (string, reversedString) { // Creo un'altra funzione per verifare se la parola è palindroma
+    if (string === revesedString){ // se la parola inverita è uguale alla parola non invertita allora:
+        return true; // Ritorno il valore true
+    } else { // Altrimenti
+        return false; // Ritorno il valore false
+    } // Fine blocco if
+} // Fine funzione
