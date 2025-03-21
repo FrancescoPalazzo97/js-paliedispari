14941851458f4e7313be926fdfab1b15820ee63f
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-21 16:11:41 +0100

    [UPD] Added 1st function reverse

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//PRIMO ESERCIZIO
+//Palindromia
+function reverseString (string){ // Creo una funzione per invertire la stringa
+    return string.reverse(); // Ritorno il valore della stringa invertito
+} // Fine funzione
