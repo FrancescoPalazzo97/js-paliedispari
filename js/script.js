f2825dc7600dec9af0cdc4b8bcfe06391764387e
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-21 16:37:59 +0100

    [UPD] 1st exercise done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 //PRIMO ESERCIZIO
 //Palindromia
 function reverseString (string){ // Creo una funzione per invertire la stringa
-    return string.reverse(); // Ritorno il valore della stringa invertito
+    return string.split('').reverse().join(''); // Ritorno il valore della stringa invertito
 } // Fine funzione
 
 function palindrome (string, reversedString) { // Creo un'altra funzione per verifare se la parola è palindroma
-    if (string === revesedString){ // se la parola inverita è uguale alla parola non invertita allora:
-        return true; // Ritorno il valore true
+    if (string === reversedString){ // se la parola inverita è uguale alla parola non invertita allora:
+        console.log(`Le parole sono palindrome`); // Stampo il risultato
     } else { // Altrimenti
-        return false; // Ritorno il valore false
+        console.log(`Le parole non sono palindrome`); // Stampo il risultato
     } // Fine blocco if
 } // Fine funzione
+
+let word = prompt(`Inserisci una parola`); // Faccio inserire la parola all'utente ramite prompt
+let reversedWord = reverseString (word); // Richiamo la funzione per invertire la stringa
+palindrome (word, reversedWord); // Richiamo la funzione
+//FINE ESERCIZIO
